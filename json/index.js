b28f84d91de5f0e941597ed4693803e2be999f3c
Glitch (rob-bot) <none>
2018-04-08 22:35:04 +0000

    🍴🍗 Checkpoint ./.gitignore:862755/446

---
+++ json/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+    grw: require('./grw.json'),
+    cfg: require('./config.json'),
+    voteSet: require('./vote-settings.json'),
+    langs: require('./languages.json'),
+};
