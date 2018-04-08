76c7e21993f3861b7c6eda2a25f7ecd33ef4b72f
Glitch (rob-bot) <none>
2018-04-08 15:48:43 +0000

    💿⛴ Checkpoint ./cmds/slap.js:862755/172

--- cmds/slap.js
+++ cmds/slap.js
@@ -37,7 +37,9 @@ module.exports = class SlapCommand {
                       dispatcher = connection.playArbitraryInput('https://cdn.glitch.com/f7bc4439-ba46-4b35-9030-a6e245debc02%2Fil_y_a_quelque_chose_d\'autre.mp3?1523201758061');
                       dispatcher.setVolume(1);
                       dispatcher.on('end', () => {
-                          tVChan.leave();
+                          setTimeout(() => {
+                            tVChan.leave();
+                          }, 2000)
                       });
                   });
               });
