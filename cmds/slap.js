e247109078f1660546da50d56741c2b86153fde4
Glitch (rob-bot) <none>
2018-04-08 14:53:43 +0000

    💳🌉 Checkpoint ./.glitch-assets:862755/791 ./cmds/slap.js:862755/1331

--- cmds/slap.js
+++ cmds/slap.js
@@ -20,12 +20,28 @@ module.exports = class SlapCommand {
         if(target == null) return;
         const tVChan = this.msg.guild.members.get(target.id).voiceChannel;
         const targetUsername = this.msg.guild.members.get(target.id).displayName;
+        const dispatcher = connection.playFile('C:/Users/Discord/Desktop/myfile.mp3');
         console.log(tVChan)
         
-        if(tVChan) {
-            tVChan.join();
-            say.speak("this is for you !");
-        }             
+      
+        dispatcher.on('end', () => {
+          // The song has finished
+        });
+
+        dispatcher.on('error', e => {
+          // Catch any errors that may arise
+          console.log(e);
+        });
+
+        dispatcher.setVolume(0.5); // Set the volume to 50%
+        dispatcher.setVolume(1); // Set the volume back to 100%
+
+        console.log(dispatcher.time); // The time in milliseconds that the stream dispatcher has been playing for
+
+        dispatcher.pause(); // Pause the stream
+        dispatcher.resume(); // Carry on playing
+
+        dispatcher.end(); // End the dispatcher, emits 'end' event
         
     }
 }
