5371be63a93b97896fe90ba14c5c50b7be85618e
Glitch (rob-bot) <none>
2018-04-08 22:06:41 +0000

    Exporting to user/repo

--- json/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-    grw: require('./grw.json'),
-    cfg: require('./config.json'),
-    voteSet: require('./vote-settings.json'),
-    langs: require('./languages.json'),
-};
