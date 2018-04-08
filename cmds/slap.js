4139e0802087db40923b9e945afd924f7db840e4
Glitch (rob-bot) <none>
2018-04-08 15:15:44 +0000

    👾🗺 Checkpoint ./cmds/slap.js:862755/3323

--- cmds/slap.js
+++ cmds/slap.js
@@ -2,6 +2,8 @@
 
 const Global = require('../global/')
 const Json = require("../json/")
+const express = require('express');
+const app = express();
 const assets = require('../assets')
 
 
@@ -22,29 +24,29 @@ module.exports = class SlapCommand {
         const text = args[0].replace(".", " ");
         if(target == null) return;
         const tVChan = this.msg.guild.members.get(target.id).voiceChannel;
-        const targetUsername = this.msg.guild.members.get(target.id).displayName;
-        const dispatcher = connection.playFile('C:/Users/Discord/Desktop/myfile.mp3');
-        console.log(tVChan)
+        //const targetUsername = this.msg.guild.members.get(target.id).displayName;
+        console.log('/assets/on_en_a_eu_un_autre.mp3')
         
+        if(tVChan) {
+          tVChan.join()
+          .then(connection => {connection.playStream("/assets", assets);
+              connection.playArbitraryInput('http://mysite.com/sound.mp3');
+          })
+        }
       
-        dispatcher.on('end', () => {
-          // The song has finished
-        });
+//         dispatcher.on('end', () => {
+//           // The song has finished
+//         });
 
-        dispatcher.on('error', e => {
-          // Catch any errors that may arise
-          console.log(e);
-        });
+//         dispatcher.setVolume(0.5); // Set the volume to 50%
+//         dispatcher.setVolume(1); // Set the volume back to 100%
 
-        dispatcher.setVolume(0.5); // Set the volume to 50%
-        dispatcher.setVolume(1); // Set the volume back to 100%
+//         console.log(dispatcher.time); // The time in milliseconds that the stream dispatcher has been playing for
 
-        console.log(dispatcher.time); // The time in milliseconds that the stream dispatcher has been playing for
+//         dispatcher.pause(); // Pause the stream
+//         dispatcher.resume(); // Carry on playing
 
-        dispatcher.pause(); // Pause the stream
-        dispatcher.resume(); // Carry on playing
-
-        dispatcher.end(); // End the dispatcher, emits 'end' event
+//         dispatcher.end(); // End the dispatcher, emits 'end' event
         
     }
 }
