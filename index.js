072dd96c72d645c6b4064ba486883a74f013d482
Glitch (rob-bot) <none>
2018-04-09 09:00:27 +0000

    💄🕹 Checkpoint ./index.js:862755/2

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ bot.on("ready", () => {
         }
     })
     .catch(console.error);
-    bot.user.setUsername("GhostBot");
+    //bot.user.setUsername("GhostBot");
 });
 
 bot.on("guildCreate", (guild) => {
