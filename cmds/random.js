300ff265ed0cab1d9e256dfc65e79c3e901865f1
Glitch (ghostbot) <none>
2018-04-15 20:04:59 +0000

    🌗🚵 Checkpoint ./index.js:862755/11 ./json/grw.json:862755/105 ./cmds/random.js:862755/57

--- cmds/random.js
+++ cmds/random.js
@@ -13,13 +13,14 @@ module.exports = class RandomCommand {
         const lang = this.guild.lang;
         const prefix = this.guild.prefix;
         const args = query.split(" ");
+        const hours = Json.grw.hours;
 
         const randomWeather = Json.grw.weather[Global.Fn.randomNumber(0, Json.grw.weather.length - 1)],
         randomMaps = Json.grw.maps[Global.Fn.randomNumber(0, Json.grw.maps.length - 1)],
         embed = {
             "title": (args[0] && args[1]) ? `[${args[0].toString()}] vs [${args[1].toString()}]` : '',
             "description": `**${randomMaps.name[lang].toUpperCase()}**
-            \n🕑 ${Global.Fn.randomNumber(0, 24)}h               ${randomWeather.emoji} ${randomWeather.name[lang]}`,
+            \n🕑 ${hoursGlobal.Fn.randomNumber(0, hours.length)}h               ${randomWeather.emoji} ${randomWeather.name[lang]}`,
             "color": Global.Fn.getMode(randomMaps.mode).color,
             "thumbnail": {
                 "url": randomMaps.url
