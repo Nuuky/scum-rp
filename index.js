6288234224ddaf215683cf4357743039c60c8a97
Glitch (scum-rp) <none>
2018-09-07 17:44:00 +0000

    🏕☔️ Checkpoint ./index.js:862755/97

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'ue strict'
+'use strict'
 
-//nst Discord = require("discord.js")
+const Discord = require("discord.js")
 const bot = new Discord.Client({autorun: true})
 
 const Json = require("./json/")
