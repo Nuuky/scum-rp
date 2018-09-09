0eb092a4406954c92650383881e4c8691d7b9e99
Glitch (scum-rp) <none>
2018-09-09 13:36:30 +0000

    🚟🌔 Checkpoint ./json/scum-data.json:862755/54 ./cmds/user/new-user.js:862755/63702

--- cmds/user/new-user.js
+++ cmds/user/new-user.js
@@ -5,7 +5,53 @@ const Global = require("../../global/");
 const ObjectId = require('mongodb').ObjectID;
 const Discord = require("discord.js")
 
+const userDefData = [
+    {data: "cancel", "name": "annuler", def: "Annuler"},
+    {data: "name", "name": "Nom", def: "Votre nom"},
+    {data: "sex", "name": "Sexe",  def: "Votre sexe"},
+    {data: "age", "name": "Age",  def: "Votre age"},
+    {data: "head", "name": "Tête",  def: "Votre tête"},
+    {data: "tatoo", "name": "Tatouage",  def: "Votre tatouage"},
+    {data: "crimes", "name": "Crime(s)",  def: "Vos crimes (Ou pas)"},
+    {data: "job", "name": "Métier",  def: "Votre métier"},
+    {data: "hostility", "name": "Hostilité",  def: "Votre hostilité"},
+    {data: "groupe", "name": "Groupe",  def: "Votre groupe"}, 
+    {data: "religion", "name": "Religion",  def: "Votre religion"},
+    {data: "description", "name": "Background",  def: "Votre description"}
+]
+
 module.exports = {
+    update: {
+        "question": () => {
+            let dataQuest = "";
+
+            userDefData.forEach((entry, index) => {
+                dataQuest += "`" + index + "`:" + entry.def + "\n"
+            })
+
+            let embed = {
+                "title": "**Modification de votre profile**",
+                "description": "Veuillez choisir l'information à modifier par son numéro.",
+                "fields": [
+                    {
+                        "name": "Réponses",
+                        "value": dataQuest
+                    }
+                ]
+            }
+            return embed
+        },
+        "answer": (msg) => {
+            let dataToUpdt;
+            if(msg.content == 0) return ["end"];
+            if(!isNaN(msg.content) && (msg.content >= 0 && msg.content <= userDefData.length - 1)) dataToUpdt = msg.content
+            if(dataToUpdt) return ["next", {data: dataToUpdt}]
+            else {
+                msg.author.send("**Erreur:** Veuillez répondre avec l'un des chiffres proposés.")
+                .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*60)})
+            }
+        }
+    },
     steps: [
         {
             "question": () => {
@@ -27,7 +73,7 @@ module.exports = {
                 } else if(msg.content.toLowerCase() == "non") {
                     return ["end"]
                 } else {
-                    msg.author.send("Erreur: Veuillez répondre avec l'un des thermes proposés.")
+                    msg.author.send("**Erreur:** Veuillez répondre avec l'un des thermes proposés.")
                     .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*60)})
                 }
             }
@@ -53,148 +99,170 @@ module.exports = {
             "question": () => {
                 let embed = {
                     "title": "**Quel est le sexe de votre personnage ?**",
-                    "description": "Répondez par un nombre",
                     "fields": [
                         {
                             "name": "Réponses",
-                            "value": "`homme` `femme`"
+                            "value": "`homme` / `femme`"
                         }
                     ]
                 }
                 return embed
             },
             "answer": (msg) => {
+                // EXEPTIONS -------
+                // DATA -------
+                // SHIELD -------
+                // ALL GOOD -------
                 if(msg.content.toLowerCase() == "homme") return ["save", {"name": "style", "inner": "sex", "content": 0, "obj": true}]
                 if(msg.content.toLowerCase() == "femme") return ["save", {"name": "style", "inner": "sex", "content": 1, "obj": true}]
-                msg.author.send("Erreur: Réponse invalide.")
+                msg.author.send("**Erreur:** Réponse invalide.")
                 .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
             }
         },
 
 
-
+        
         {
             "question": () => {
                 let embed = {
                     "title": "**Quel age a votre personnage ?**",
-                    "description": "Ecrivez uniquement le chiffre, n'ajoutez pas \"ans\"",
+                    "description": "Ecrivez uniquement des chiffres.",
                 }
                 return embed
             },
             "answer": (msg) => {
-                if(!isNaN(msg.content) && (msg.content >= 20 && msg.content <= 50)) return ["save", {"name": "age", "content": msg.content}]
-                msg.author.send("Erreur: Réponse invalide.")
-                .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // EXEPTIONS -------
+                // DATA -------
+                // SHIELD -------
+                if(isNaN(msg.content) && !(msg.content >= 20 && msg.content <= 50)) return msg.author.send("**Erreur:** Réponse invalide.")
+                    .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                    // ALL GOOD -------
+                return ["save", {"name": "age", "content": msg.content}]
+                
             }
         },
 
 
-
+        
         {
             "question": () => {
+                let heads = ""
+                Json.scumData.heads.forEach((head, index) => {
+                    heads += `\`${index}\`: ${head}\n`
+                })
                 let embed = {
                     "title": "**Quel tête avez-vous choisis ?**",
-                    "description": "Répondez par un nombre",
+                    "description": "Répondez avec l'index de la tête.",
                     "fields": [
                         {
                             "name": "Réponses",
-                            "value": "`1` -> `4`"
+                            "value": heads
                         }
                     ]
                 }
                 return embed
             },
             "answer": (msg) => {
-                if(!isNaN(msg.content) && (msg.content >= 1 && msg.content <= 4)) return ["save", {"name": "style", "inner": "head", "content": msg.content - 1, "obj": true}]
-                msg.author.send("Erreur: Réponse invalide.")
+                // EXEPTIONS -------
+                // DATA -------
+                const heads = Json.scumData.heads
+                // SHIELD -------
+                if(isNaN(msg.content) || !(msg.content >= 0 && msg.content <= (heads.length + 1))) return msg.author.send("**Erreur:** Réponse invalide.")
                 .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // ALL GOOD -------
+                return ["save", {"name": "style", "inner": "head", "content": heads[msg.content] - 1, "obj": true}]
             }
         },
 
 
-
+        
         {
             "question": () => {
+                let tatoos = ""
+                Json.scumData.tatoos.forEach((tatoo, index) => {
+                    tatoos += `\`${index}\`: ${tatoo}\n`
+                })
+
                 let embed = {
                     "title": "**Quel est votre tatouage ?**",
-                    "description": "Répondez par un nombre\n⚠️ Si vous n'avez pas de tatouage c'est 0.\n⚠️ Le premium est le numéro 1",
+                    "description": "Répondez avec l'index du tatouage.",
                     "fields": [
                         {
                             "name": "Réponses",
-                            "value": "`0` -> `4`"
+                            "value": tatoos
                         }
                     ]
                 }
                 return embed
             },
             "answer": (msg) => {
-                if(!isNaN(msg.content) && (msg.content >= 0 && msg.content <= 4)) return ["save", {"name": "style", "inner": "tatoo", "content": msg.content, "obj": true}]
-                msg.author.send("Erreur: Réponse invalide.")
-                .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // EXEPTIONS -------
+                // DATA -------
+                const tatoos = Json.scumData.tatoos;
+                // SHIELD -------
+                if(isNaN(msg.content) || !(msg.content >= 0 && msg.content <= (tatoos.length + 1))) return msg.author.send("**Erreur:** Réponse invalide.")
+                    .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // ALL GOOD -------
+                return ["save", {"name": "style", "inner": "tatoo", "content": msg.content, "obj": true}]
+                
             }
         },
 
 
-
+        
         {
             "question": () => {
+                let crimes = ""
+                Json.scumData.crimes.forEach((crime, index) => {
+                    crimes += `\`${index}\`: ${crime}\n`
+                })
                 let embed = {
                     "title": "**Quel sont vos crimes ?** *(Optionnel)*",
-                    "description": "Séparez chaque crimes par une virgules (2 crimes max)\nTappez `skip` si vous êtes innocent.",
+                    "description": "Séparez chaque crimes par une virgules (2 crimes max)\nRépondez avec l'index du crime.",
                     "fields": [
-                        
+                        {
+                            "name": "Réponses",
+                            "value": crimes
+                        }
                     ]
                 }
-                let crimes = ""
-                Json.scumData.crimes.forEach(crime => {
-                    crimes += "`" + crime + "` "
-                });
-                const field = {
-                    "name": "Réponses",
-                    "value": crimes
-                } 
-                embed.fields.push(field)
-
                 return embed
             },
             "answer": (msg) => {
-                let crimes = msg.content.replace(" ", "")
-                crimes = crimes.split(",")
-
+                // EXEPTIONS -------
                 if(msg.content.toLowerCase() == "skip") return ["skip"]
-
-                if(crimes.length <= 2) {
-                    crimes.forEach((msgCrime) => {
-                        let exist = false
-                        Json.scumData.crimes.forEach(crime => {
-                            if(msgCrime.toLowerCase() == crime.toLowerCase()) {
-                                exist = true;
-                            }
-                        })
-                        if(!exist) {
-                            return msg.author.send("Erreur: Crime(s) non valide(s).")
-                            .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
-                        }
-                    })
-                    return ["save", {"name": "crimes", "content": crimes}]
-                }
-                msg.author.send("Erreur: Vous avez séléctionnez trop de crimes.")
+                // DATA -------
+                let answArr = msg.content.replace(" ", "")
+                answArr = answArr.split(",")
+                const crimes = Json.scumData.crimes
+                // SHIELD -------
+                if(answArr.length > 2) return msg.author.send("**Erreur:** Vous avez selectionné trop de crimes.")
                 .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                answArr.forEach(crime => {
+                    if(isNaN(crime) || !(crime >= 0 && crime <= (crimes.length + 1)) ) return msg.author.send("**Erreur:** Crime invalide.")
+                    .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                })
+                // ALL GOOD -------
+                let finalAnsw = [];
+                answArr.forEach(crime => {
+                    finalAnsw.push(crimes[crime])
+                })
+                return ["save", {"name": "crimes", "content": finalAnsw}]
             }
         },
 
 
-
+        
         {
             "question": () => {
                 let jobs = ""
-                Json.scumData.jobs.forEach(job => {
-                    jobs += "`" + job + "` "
+                Json.scumData.jobs.forEach((job, index) => {
+                    jobs += `\`${index}\`: ${job}\n`
                 })
 
                 let embed = {
                     "title": "**Quel est votre métier ?** *(Optionnel)*",
-                    "description": "Tappez `skip` si vous n'avez pas encore choisis de métier.",
+                    "description": "Répondez avec l'index du métier.",
                     "fields": [
                         {
                             "name": "Réponses",
@@ -205,52 +273,55 @@ module.exports = {
                 return embed
             },
             "answer": (msg) => {
+                // EXEPTIONS -------
                 if(msg.content.toLowerCase() == "skip") return ["skip"]
-
-                let exist = false;
-                Json.scumData.jobs.forEach(job => {
-                    if(msg.content.toLowerCase() == job.toLowerCase()) exist = true
-                })
-
-                if(exist) return ["save", {"name": "job", "content": msg.content}]
-
-                msg.author.send("Erreur: Le métier choisis est invalide.")
+                // DATA -------
+                const jobs = Json.scumData.jobs 
+                // SHIELD -------
+                if(isNaN(msg.content) || !(msg.content >= 0 && msg.content <= (jobs.length + 1)) ) return msg.author.send("**Erreur:** Métier invalide.")
                 .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // ALL GOOD -------
+                return ["save", {"name": "job", "content": jobs[msg.content]}]
             }
         },
 
 
-
+        
         {
             "question": () => {
+                let hostilities = "";
+
+                Json.scumData.hostilities.forEach((host, index) => {
+                    hostilities += `\`${index}\`: ${host}\n`
+                })
+
                 let embed = {
                     "title": "**Quel est votre degré d'hostilé ?**",
+                    "description": "Répondez avec l'index de la réponse.",
                     "fields": [
                         {
                             "name": "Réponses",
-                            "value": "`Amicale` `Méfiant` `Hostile`"
+                            "value": hostilities
                         }
                     ]
                 }
                 return embed
             },
             "answer": (msg) => {
-                let host = ["amicale", "méfiant", "hostile"]
+                // EXEPTIONS -------
+                // DATA -------
+                const hostilities = Json.scumData.hostilities
+                // SHIELD -------
+                if(isNaN(msg.content) || !(msg.content >= 0 && msg.content <= (hostilities.length + 1))) return msg.author.send("**Erreur:** Hostilité invalide.")
+                    .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // ALL GOOD -------
+                return ["save", {"name": "hostility", "content": hostilities[msg.content]}]
 
-                let exist = false;
-                host.forEach(h => {
-                    if(msg.content.toLowerCase() == h) exist = true;
-                })
-
-                if(exist) return ["save", {"name": "hostility", "content": msg.content}]
-
-                msg.author.send("Erreur: Hostilité invalide.")
-                .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
             }
         },
 
 
-
+        
         {
             "question": () => {
                 return Global.Fn.waitFor(Global.Fn.findData("find", "groupe_info", {}))
@@ -273,22 +344,25 @@ module.exports = {
                 })
             },
             "answer": (msg) => {
+                // EXEPTIONS -------
                 if(msg.content.toLowerCase() == "skip") return ["skip"]
-
+                // DATA -------
                 return Global.Fn.waitFor(Global.Fn.findData("findOne", "groupe_info", {"name": msg.content.toLowerCase()}))
                 .then(groupe => {
-                    console.log("Groupe Pending: ",groupe.pending)
-                    const makeCtt = { "id": groupe._id, "pending": (groupe.pending + 1) }
-                    if(groupe) return ["save", {"name": "groupe", "content": makeCtt}]
-
-                    msg.author.send("Erreur: Le groupe indiqué n'éxiste pas.")
+                    // SHIELD -------
+                    if(typeof groupe == 'undefined') msg.author.send("**Erreur:** Le groupe indiqué n'existe pas.")
                     .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                    // ALL GOOD -------
+                    const makeCtt = (groupe) ? "pending" : false
+                    return ["save", {"name": "groupe", "content": makeCtt}]
+
+                    
                 })
             }
         },
 
 
-
+        
         {
             "question": () => {
                 return Global.Fn.waitFor(Global.Fn.findData("find", "religion_info", {}))
@@ -312,21 +386,23 @@ module.exports = {
                 })
             },
             "answer": (msg) => {
+                // EXEPTIONS -------
                 if(msg.content.toLowerCase() == "skip") return ["skip"]
-
+                // DATA -------
                 return Global.Fn.waitFor(Global.Fn.findData("findOne", "religion_info", {"name": msg.content.toLowerCase()}))
                 .then(religion => {
-                    const makeCtt = { "id": religion._id, "pending": (religion.pending + 1) }
-                    if(religion) return ["save", {"name": "religion", "content": makeCtt}]
-
-                    msg.author.send("Erreur: La religion indiqué n'éxiste pas.")
+                    // SHIELD -------
+                    if(typeof religion == 'undefined') return msg.author.send("**Erreur:** La religion indiqué n'éxiste pas.")
                     .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                    // ALL GOOD -------
+                    const makeCtt = (religion) ? "pending" : false
+                    return ["save", {"name": "religion", "content": makeCtt}]
                 })
             }
         },
 
 
-
+        
         {
             "question": () => {
                 let embed = {
@@ -336,11 +412,14 @@ module.exports = {
                 return embed
             },
             "answer": (msg) => {
+                // EXEPTIONS -------
                 if(msg.content.toLowerCase() == "skip") return ["end", true]
-                if(msg.content.length <= 1024) return ["save", {"name": "background", "content": msg.content}]
-
-                msg.author.send("Erreur: Votre texte est trop long.\nVous devez enlever `" + msg.content.length - 1024 + "` caractères.")
+                // DATA -------
+                // SHIELD -------
+                if(msg.content.length > 1024) return msg.author.send("**Erreur:** Votre texte est trop long.\nVous devez enlever `" + msg.content.length - 1024 + "` caractères.")
                 .then(omsg => {setTimeout(() => {omsg.delete()}, 1000*5)})
+                // ALL GOOD -------
+                return ["save", {"name": "description", "content": msg.content}]
             }
         }
     ]
