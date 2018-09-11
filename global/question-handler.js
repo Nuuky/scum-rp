40ad2e0b57c57fc0af421d1245bdfc76f46529be
Glitch (scum-rp) <none>
2018-09-11 16:03:02 +0000

    💍👙 Checkpoint ./global/question-handler.js:862755/24935

--- global/question-handler.js
+++ global/question-handler.js
@@ -12,36 +12,36 @@ module.exports = class QuestionHandler {
             createData = (mongoAction == "create") ? true : false,
             questIndex = (createData) ? 1 : 0,
             questNumber = 0,
-            questToDo = (createData) ? userQuest.steps.length : 2;
+            questToDo = (createData) ? userQuest.steps.length : 2,
+            groupe, religion;
+
+            const User = (createData) ? null : Fn.waitFor(Fn.findData("findOne", mongoColl, {_id: msg.author.id}))
+                .then(user => {
+                    groupe = Fn.findData("findOne", "groupe_info", {_id: user.groupe});
+                    religion = Fn.findData("findOne", "religion_info", {_id: user.religion});
+                })
+
       
         let embed = userQuest.steps[questIndex].question()
         msg.author.send({embed})
         .then(omsg => {    
-            const questCollector = new Discord.MessageCollector(omsg.channel, m => m.author.id === msg.author.id, { time: 10000*60*60 });
-            console.log("Collector created !")
+            const questCollector = new Discord.MessageCollector(omsg.channel, m => m.author.id === msg.author.id, { time: 10000*60*15 });
             questCollector.on("collect", message => {
-                console.log("Collecting Quest " + (questIndex + 1) + " of " + questToDo + "...")
     
                 // User canceled
-                if(message == "stop") {
-                    console.log("Canceling...")
-                    return questCollector.stop("canceled");
-                }
-                console.log(questIndex);
+                if(message == "stop") return questCollector.stop("canceled");
+                
+                
                 Fn.waitFor(userQuest.steps[questIndex].answer(message))
                     .then((obj) => {
-                        console.log("Treating answer...")
-                  
                         
-                        console.log("case: ", obj[0])
                         switch(obj[0]) {
                             case "skip":
                                 if(obj[1].obj) {
                                     if(!objColl[obj[1].name]) objColl[obj[1].name] = {}
                                     objColl[obj[1].name][obj[1].inner] = obj[1].content;
-                                } else {
-                                    objColl[obj[1].name] = obj[1].content;
-                                }
+                                } else objColl[obj[1].name] = obj[1].content;
+
                                 questNumber++
                                 questIndex++
                                 if(questNumber >= (questToDo - 1))  return questCollector.stop("save");
@@ -54,7 +54,6 @@ module.exports = class QuestionHandler {
                                 break;
 
                             case "next":
-                                console.log("obj[1]: ", obj[1])
                                 questIndex = obj[1].dataIndex
                                 questNumber++
                                 Fn.waitFor(userQuest.steps[questIndex].question())
@@ -82,53 +81,40 @@ module.exports = class QuestionHandler {
             questCollector.on("end", (collected, reason) => {
                 if(reason == "save") {
                     if(!createData) {
-                        Fn.waitFor(Fn.findData("findOne", mongoColl, {_id: msg.author.id}))
-                        .then(User => {
-                            if((User.groupe && objColl.groupe) && User.groupe != objColl.groupe) {
-                                Fn.waitFor(Fn.findData("findOne", "groupe_info", {_id: User.groupe}))
-                                .then(grp => {
-                                    let members = grp.members
-                                    for( var i = 0; i < members.length-1; i++){ 
-                                        if ( members[i].id == msg.author.id ) {
-                                            members.splice(i, 1); 
-                                        }
+                        if((User.groupe && objColl.groupe) && User.groupe != objColl.groupe) {
+                                let members = groupe.members
+                                for( var i = 0; i < members.length-1; i++){ 
+                                    if ( members[i].id == msg.author.id ) {
+                                        members.splice(i, 1); 
                                     }
-                                    Fn.mongUpdate({_id: User.groupe}, "update", "groupe_info", { $set:{"members":members} })
-                                })
-                            }
-                            
-                            if((User.religion && objColl.religion) && User.religion != objColl.religion) {
-                                Fn.waitFor(Fn.findData("findOne", "religion_info", {_id: User.religion}))
-                                .then(grp => {
-                                    let members = grp.members
-                                    for( var i = 0; i < members.length-1; i++){ 
-                                        if ( members[i] == msg.author.id ) {
-                                            members.splice(i, 1); 
-                                        }
+                                }
+                                Fn.mongUpdate({_id: User.groupe}, "update", "groupe_info", { $set:{"members":members} })
+                        }
+                        
+                        if((User.religion && objColl.religion) && User.religion != objColl.religion) {
+                                let members = religion.members
+                                for( var i = 0; i < members.length-1; i++){ 
+                                    if ( members[i] == msg.author.id ) {
+                                        members.splice(i, 1); 
                                     }
-                                    Fn.mongUpdate({_id: User.religion}, "update", "religion_info", { $set:{"members":members} })
-                                })
-                            }
-                            console.log("objColl: ", objColl)
-                            Fn.mongUpdate({_id: msg.author.id}, "update", mongoColl, {$set: objColl})
-                        })
+                                }
+                                Fn.mongUpdate({_id: User.religion}, "update", "religion_info", { $set:{"members":members} })
+                        }
+                        Fn.mongUpdate({_id: msg.author.id}, "update", mongoColl, {$set: objColl})
                     }
 
                     if(createData) Fn.mongUpdate(objColl, mongoAction, mongoColl)
 
                     const newPendingMember = { $addToSet: {members: {id: msg.author.id, pending: true}}}
+
                     // Update Groupe DB
                     if(objColl.groupe) Fn.mongUpdate({_id: objColl.groupe}, "update", "groupe_info", newPendingMember)
 
                     // Update Religion DB
                     if(objColl.religion) Fn.mongUpdate({_id: objColl.religion}, "update", "religion_info", newPendingMember)
 
-                    embed = {
-                        "title": "**Succès !**",
-                        "description": `Les informations ont bien été prises en comptes.`
-                    }
-
-                    return msg.author.send({embed})
+                    // End the collection
+                    return msg.author.send({embed: {"title": "**Succès !**", "description": `Les informations ont bien été prises en comptes.`}})
                 }
               
                 msg.author.send("Action annulé.")
