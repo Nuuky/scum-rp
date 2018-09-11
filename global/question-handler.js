9b0d9a8e10f0c2654cadc90bc99cc51cb3ca5853
Glitch (scum-rp) <none>
2018-09-11 15:21:05 +0000

    💊🛥 Checkpoint ./global/question-handler.js:862755/13715

--- global/question-handler.js
+++ global/question-handler.js
@@ -7,7 +7,7 @@ const Discord = require("discord.js")
 
 module.exports = class QuestionHandler {
 
-    static run(msg, userQuest, mongoColl, mongoAction = "create") {
+    async run(msg, userQuest, mongoColl, mongoAction = "create") {
         let objColl = {},
             createData = (mongoAction == "create") ? true : false,
             questIndex = (createData) ? 1 : 0,
