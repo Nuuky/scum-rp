f75eb4e2d7d768ef10aa3dbd52367b92a21adc90
Glitch (scum-rp) <none>
2018-09-11 18:22:59 +0000

    🎗🐑 Checkpoint ./global/question-handler.js:862755/12759

--- global/question-handler.js
+++ global/question-handler.js
@@ -20,48 +20,52 @@ module.exports = class QuestionHandler {
                     groupe = Fn.findData("findOne", "groupe_info", {_id: user.groupe});
                     religion = Fn.findData("findOne", "religion_info", {_id: user.religion});
                 })
+                .catch(e => console.error(e))
 
       
         let embed = userQuest.steps[questIndex].question()
         msg.author.send({embed})
-        .then(omsg => {    
+        .then(omsg => {
             const questCollector = new Discord.MessageCollector(omsg.channel, m => m.author.id === msg.author.id, { time: 10000*60*15 });
             questCollector.on("collect", message => {
     
                 // User canceled
                 if(message == "stop") return questCollector.stop("canceled");
                 
-                
+                // Make an obj with the user answer
                 Fn.waitFor(userQuest.steps[questIndex].answer(message))
                     .then((obj) => {
                         
                         switch(obj[0]) {
                             case "skip":
-                                if(obj[1].obj) {
+                                // Save data in objColl and check if need to end the collection
+                                if(obj[1].obj) { // If obj in obj
                                     if(!objColl[obj[1].name]) objColl[obj[1].name] = {}
                                     objColl[obj[1].name][obj[1].inner] = obj[1].content;
                                 } else objColl[obj[1].name] = obj[1].content;
 
+                                // 
                                 questNumber++
                                 questIndex++
                                 if(questNumber >= (questToDo - 1))  return questCollector.stop("save");
                                 Fn.waitFor(userQuest.steps[questIndex].question())
                                 .then(emd => {
                                     msg.author.send({embed: emd})
-                                    .catch(err => console.error(err))
+                                    .catch(e => console.error(e))
                                 })
-                                .catch(err => console.error(err))
+                                .catch(e => console.error(e))
                                 break;
 
                             case "next":
+                                // First step update (To get the Question / Answer index)
                                 questIndex = obj[1].dataIndex
                                 questNumber++
                                 Fn.waitFor(userQuest.steps[questIndex].question())
                                 .then(emd => {
                                     msg.author.send({embed: emd})
-                                    .catch(err => console.error(err))
+                                    .catch(e => console.error(e))
                                 })
-                                .catch(err => console.error(err))
+                                .catch(e => console.error(e))
                                 break;
     
                             case "end":
@@ -72,26 +76,31 @@ module.exports = class QuestionHandler {
                                 break;
     
                             default:
+                                // Wrong answer
                                 break;
                         }
                     })
-                    .catch(err => console.error(err))
+                    .catch(e => console.error(e))
     
             })
             questCollector.on("end", (collected, reason) => {
                 if(reason == "save") {
+                    // If need to save data in DB
                     if(!createData) {
+                        // In case of Update
                         if((User.groupe && objColl.groupe) && User.groupe != objColl.groupe) {
-                                let members = groupe.members
-                                for( var i = 0; i < members.length-1; i++){ 
-                                    if ( members[i].id == msg.author.id ) {
-                                        members.splice(i, 1); 
-                                    }
+                            // Check if need to delete user from his old groupe
+                            let members = groupe.members
+                            for( var i = 0; i < members.length-1; i++){ 
+                                if ( members[i].id == msg.author.id ) {
+                                    members.splice(i, 1); 
                                 }
-                                Fn.mongUpdate({_id: User.groupe}, "update", "groupe_info", { $set:{"members":members} })
+                            }
+                            Fn.mongUpdate({_id: User.groupe}, "update", "groupe_info", { $set:{"members":members} })
                         }
                         
                         if((User.religion && objColl.religion) && User.religion != objColl.religion) {
+                            // Check if need to delete user from his old religion
                                 let members = religion.members
                                 for( var i = 0; i < members.length-1; i++){ 
                                     if ( members[i] == msg.author.id ) {
@@ -100,29 +109,26 @@ module.exports = class QuestionHandler {
                                 }
                                 Fn.mongUpdate({_id: User.religion}, "update", "religion_info", { $set:{"members":members} })
                         }
+                        // Update User profile
                         Fn.mongUpdate({_id: msg.author.id}, "update", mongoColl, {$set: objColl})
                     }
-
+                    // Create User profile
                     if(createData) Fn.mongUpdate(objColl, mongoAction, mongoColl)
 
-                    const newPendingMember = { $addToSet: {members: {id: msg.author.id, pending: true}}}
-
-                    // Update Groupe DB
-                    if(objColl.groupe) Fn.mongUpdate({_id: objColl.groupe}, "update", "groupe_info", newPendingMember)
-
-                    // Update Religion DB
-                    if(objColl.religion) Fn.mongUpdate({_id: objColl.religion}, "update", "religion_info", newPendingMember)
+                    // Push user into ether groupe or religion DB with pending true for groupe (Groupe leader need to approuve user)
+                    if(objColl.groupe) Fn.mongUpdate({_id: objColl.groupe}, "update", "groupe_info", { $addToSet: {members: {id: msg.author.id, pending: true}}}) // Update Groupe DB
+                    if(objColl.religion) Fn.mongUpdate({_id: objColl.religion}, "update", "religion_info", { $addToSet: {members: msg.author.id}}) // Update Religion DB
 
                     // End the collection
                     return msg.author.send({embed: {"title": "**Succès !**", "description": `Les informations ont bien été prises en comptes.`}})
                 }
-              
+                
+                // Collection canceled
                 msg.author.send("Action annulé.")
-              
                 console.log("Ended.")
             })
         })
-        .catch(err => console.error(err))
+        .catch(e => console.error(e))
       
     };
 }
