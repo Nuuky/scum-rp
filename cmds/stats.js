db43b5343eb536c640df0a5bf453dd6711612f86
Glitch (scum-rp) <none>
2018-09-12 00:25:02 +0000

    🙇🎷 Checkpoint ./cmds/stats.js:862755/1564

--- cmds/stats.js
+++ cmds/stats.js
@@ -9,6 +9,41 @@ const fetch = require('node-fetch');
 
 module.export = class StatsCommand {
     constructor(msg, bot) {
-        Global.Fn.waitFor(Global.Fn.findData("find", "groupe_info", {}))
+        this.msg = msg;
+        this.bot = bot;
+        this.users = Global.Fn.findData("find", "users_info", {})
+        this.groupes = Global.Fn.findData("find", "groupe_info", {})
+        this.religions = Global.Fn.findData("find", "religions_info", {})
+    }
+  
+    async run(query) {
+      const msg = this.msg;
+      const bot = this.bot;
+      const users = this.users;
+      const groupes = this.groupes;
+      const religions = this.religions;
+      
+      const embed = {
+        "title": "Stats Bot",
+        "fields": [
+          {
+            "name": "Joueurs",
+            "value": users.length,
+            "inline": true
+          },
+          {
+            "name": "Groupes",
+            "value": groupes.length,
+            "inline": true
+          },
+          {
+            "name": "Religions",
+            "value": religions.length,
+            "inline": true
+          },
+        ]
+      }
+      
+      Global.Msg.embed(embed)
     }
 }
