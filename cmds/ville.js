9312728de8d09e8c3a9d025c754809b25eb9ac22
Glitch (scum-rp) <none>
2018-09-13 13:17:12 +0000

    ⛩💐 Checkpoint ./json/cities.json:862755/332 ./global/question-handler.js:862755/2 ./cmds/ville.js:862755/5870

---
+++ cmds/ville.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const Json = require('../json/');
+const Global = require("../global/");
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGODB;
+const fetch = require('node-fetch');
+const ObjectId = require('mongodb').ObjectID;
+
+module.exports = class VilleCommand {
+
+    constructor(bot, msg) {
+        this.msg = msg;
+        this.bot = bot; 
+    }
+
+    async run(query) {
+        const args = query.split(" ");
+        const msg = this.msg;
+        const bot = this.bot;
+
+
+        // SEARCH USER --------
+        if(query) {
+            //console.log(query)
+          
+
+             // GET USER GRP -------
+            Global.waitFor(Global.Fn.findData("findOne", "groupe_info", {leader: msg.author.id}))
+            .then(groupe => {
+                if(groiupe) {
+                    return User.Search.run(msg, user);
+                }
+                return Global.Msg.send(msg, "Aucun joueur trouvé.", 60);
+            })
+          .catch(err => console.error(err))
+          
+          
+        } else {
+          
+          
+            let authorID = msg.author.id;
+            authorID = authorID.replace("<", "");
+            authorID = authorID.replace(">", "");
+          
+            const getUser = () => {
+                const promise = new Promise((resolve, reject) => {
+                    resolve(Global.Fn.findData("findOne", "user_info", {_id: authorID}))
+                })
+                return promise
+            }
+            getUser()
+            .then(user => {
+                if(user) Global.qHand.run(bot, msg, User.Questions, "user_info", "update")
+                else Global.qHand.run(bot, msg, User.Questions, "user_info")
+            })
+            .catch(err => console.error(err))
+          
+          
+        }
+    };
+}
