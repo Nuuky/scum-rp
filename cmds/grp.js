132e6d37bb8da318208de1a595b0f76d9dd06843
Glitch (scum-rp) <none>
2018-09-14 16:31:22 +0000

    🍌🐬 Checkpoint ./cmds/grp.js:862755/106

--- cmds/grp.js
+++ cmds/grp.js
@@ -25,7 +25,7 @@ module.exports = class GrpCommand {
         if(query) {
             console.log(query)
           
-            if(args[0] == "accept") 
+            if(args[0].match("add|rm")) return
           
 
             // Search for Groupe -------
