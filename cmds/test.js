d0c11feeac1e52843ad58b766a9f901580fec0fe
Glitch (scum-rp) <none>
2018-09-14 15:49:06 +0000

    🦁🚇 Checkpoint ./cmds/test.js:862755/229

--- cmds/test.js
+++ cmds/test.js
@@ -7,7 +7,8 @@ const MongoClient = require('mongodb').MongoClient;
 const url = process.env.MONGODB;
 const ObjectId = require('mongodb').ObjectID;
 const base64Img = require('base64-img');
-const Canvas = require('canvas')
+const Canvas = require('canvas');
+const fs = require('fs');
 
 module.exports = class TestCommand {
 
@@ -17,6 +18,7 @@ module.exports = class TestCommand {
     }
 
     async run(query) {
+        const args = query.split(" ");
         const msg = this.msg
         const bot = this.bot;
       
@@ -25,14 +27,14 @@ module.exports = class TestCommand {
         // })
       
       const Image = Canvas.Image,
-          canvas = Canvas.createCanvas(200, 200),
+          canvas = Canvas.createCanvas(400, 300),
           ctx = canvas.getContext('2d');
 
       ctx.font = '30px Impact';
       ctx.rotate(.1);
-      ctx.fillText("Awesome!", 50, 100);
+      ctx.fillText(query, 50, 100);
 
-      var te = ctx.measureText('Awesome!');
+      var te = ctx.measureText(query);
       ctx.strokeStyle = 'rgba(0,0,0,0.5)';
       ctx.beginPath();
       ctx.lineTo(50, 102);
@@ -42,17 +44,17 @@ module.exports = class TestCommand {
       base64Img.img(canvas.toDataURL(), './images/', '1', function(err, filepath) {
           if(err) return console.error(err);
         
-        msg.channel.send({files: ["./images/1.png"]});
+            msg.channel.send({files: ["./images/1.png"]});
         
         
-           fs.stat('./server/upload/my.csv', function (err, stats) {
-               console.log(stats);//here we got all information of file in stats variable
+           fs.stat('./images/1.png', function (err, stats) {
+               // console.log(stats);//here we got all information of file in stats variable
 
                if (err) {
                    return console.error(err);
                }
 
-               fs.unlink('./server/upload/my.csv',function(err){
+               fs.unlink('./images/1.png',function(err){
                     if(err) return console.log(err);
                     console.log('file deleted successfully');
                });  
